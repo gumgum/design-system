993e48419b6b8ac084437af98920a6b383bf7a02
Garrett Whisten <garrett@gumgum.com>
2021-10-20 11:22:50 -0700

    atoms demo build

--- pages/atoms/[id].js
+++ pages/atoms/[id].js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { Card, CardBlock, Column, Divider, Row } from "gumdrops";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
@@ -12,35 +13,29 @@ import { getAtomData } from "../../utils/atoms";
 
 export default function DocsPage({ docData }) {
   const [viewError, setViewErrow] = useState(false);
-  const { gumdrops, component, ...data } = getAtomData(docData.id);
-  const [code, setCode] = useState(component);
-
-  const formatOnKey = (e) => {
-    if (e.ctrlKey && e.keyCode == 76) {
-      setCode((currentCode) =>
-        prettier.format(currentCode, {
-          parser: "babel",
-          plugins: [babelParser],
-        })
-      );
-    }
-  };
+  const [data, setData] = useState();
+  const [code, setCode] = useState(data);
 
   useEffect(() => {
-    document.addEventListener("keyup", formatOnKey, false);
-    return () => document.removeEventListener("keyup", formatOnKey, false);
-  });
+    let response = getAtomData(docData.id);
+    if (response) {
+      setData(response);
+      setCode(response.code);
+    } else {
+      setData(response); // undefined
+    }
+  }, [docData.id]);
 
   return (
-    <div>
+    <div style={{ minHeight: "calc(100vh - 113px)" }}>
       <Head>
         <title>{docData.title}</title>
       </Head>
       <div className="gds-flex gds-flex--justify-between">
         <PageTitle title={docData.title} />
-        {gumdrops && (
+        {data && (
           <a
-            href={gumdrops}
+            href={data.gumdrops}
             target="_blank"
             rel="noref noreferrer"
             className="gds-text--link"
@@ -50,12 +45,15 @@ export default function DocsPage({ docData }) {
         )}
       </div>
       <Row>
-        <Column md="8">
+        <Column lg="8">
           {/* description */}
-          <div dangerouslySetInnerHTML={{ __html: docData.contentHtml }} />
-          <div className="gds-text--header-xs -m-b-2">Demo</div>
+          <div
+            className="-m-b-3"
+            dangerouslySetInnerHTML={{ __html: docData.contentHtml }}
+          />
           {code && (
             <LiveProvider theme={dracula} code={code}>
+              <div className="gds-text--header-xs -m-b-2">Demo</div>
               <Card>
                 <CardBlock>
                   <LivePreview />
@@ -65,7 +63,7 @@ export default function DocsPage({ docData }) {
               <Divider
                 callback={() => setViewErrow(!viewError)}
                 collapsible={true}
-                label="View Error Log"
+                label="Error Log"
                 open={viewError}
               />
               {viewError && (
@@ -77,27 +75,46 @@ export default function DocsPage({ docData }) {
             </LiveProvider>
           )}
         </Column>
-        <Column md="4">
-          <Card>
-            <CardBlock>
-              {data.variants.map((variant, i) => {
-                const options = variant.values;
-                return (
-                  <div key={i} className="-m-b-3">
-                    <Divider label={variant.title} />
-                    <div className="gds-text--body-md -m-b-2"></div>
-                    {options.map((option) => (
-                      <CopyText
-                        copyText={option}
-                        key={option}
-                        className="-m-b-2"
-                      />
-                    ))}
-                  </div>
-                );
-              })}
-            </CardBlock>
-          </Card>
+        <Column lg="4">
+          {data && (
+            <>
+              <Card style={{ maxHeight: "50vh", overflowY: "scroll" }}>
+                <CardBlock>
+                  {data.variants.map((variant, i) => {
+                    const options = variant.values;
+                    return (
+                      <div key={i} className="-m-b-3">
+                        <Divider label={variant.title} />
+                        <div className="gds-text--body-md -m-b-2"></div>
+                        {options.map((option) => (
+                          <CopyText
+                            copyText={option}
+                            key={option}
+                            className="-m-b-2"
+                          />
+                        ))}
+                      </div>
+                    );
+                  })}
+                </CardBlock>
+              </Card>
+
+              {data.related.length > 0 && (
+                <Card style={{ maxHeight: "30vh", overflowY: "scroll" }}>
+                  <CardBlock>
+                    <Divider label="Related Items" />
+                    {data.related.map((item) => {
+                      return (
+                        <Link key={item.title} href={item.url}>
+                          <a className="gds-tag gds-tag--sm">{item.title}</a>
+                        </Link>
+                      );
+                    })}
+                  </CardBlock>
+                </Card>
+              )}
+            </>
+          )}
         </Column>
       </Row>
     </div>
--- utils/atoms.js
+++ utils/atoms.js
@@ -1,11 +1,30 @@
-export const getAtomData = () => {
-  return Atoms.find(({ item }) => item === item);
+export const getAtomData = (id) => {
+  let obj = Atoms.find((item) => item.id === id);
+  return obj;
 };
 
-export const Atoms = [
+const blankStructure = {
+  id: "",
+  gumdrops: "",
+  variants: [
+    {
+      title: "",
+      values: [],
+    },
+  ],
+  related: [
+    {
+      title: "",
+      url: "",
+    },
+  ],
+
+  code: ``,
+};
+
+const Atoms = [
   {
     id: "badge-component",
-    title: "Badge",
     gumdrops: "https://gumdrops.gumgum.com/?path=/story/atoms-badge--default",
     variants: [
       {
@@ -29,11 +48,11 @@ export const Atoms = [
     ],
     related: [
       {
-        title: "Button Group",
-        url: "/",
+        title: "Tag",
+        url: "/atoms/tag-component",
       },
     ],
-    component: `
+    code: `
     <div>
       <p>Default badge<span className="gds-badge">2</span></p>
       <p>Inverse badge<span className="gds-badge gds-badge--inverse">2</span></p>
@@ -47,8 +66,8 @@ export const Atoms = [
     `,
   },
   {
-    title: "ButtonDemo",
-    id: "example-component",
+    id: "button-component",
+    gumdrops: "https://gumdrops.gumgum.com/?path=/story/atoms-button--default",
     variants: [
       {
         title: "Context (Color)",
@@ -58,28 +77,154 @@ export const Atoms = [
           "gds-button--success",
           "gds-button--danger",
           "gds-button--warning",
+          "gds-button--secondary",
+          "gds-button--white",
+          "gds-button--tertiary",
+          "gds-button--info",
+          "gds-button--link",
+          "gds-button--toggle",
+          "gds-button--dark",
+          "gds-button--dark-primary",
+          "gds-button--dark-secondary",
+          "gds-button--dark-tertiary",
+          "gds-button--dark-success",
+          "gds-button--dark-danger",
+          "gds-button--dark-info",
+          "gds-button--dark-warning",
+          "gds-button--dark-toggle",
         ],
       },
       {
         title: "Size",
-        values: ["gds-button--xs", "gds-button--sm", "gds-button--md"],
+        values: [
+          "gds-button--xs",
+          "gds-button--sm",
+          "gds-button--md",
+          "gds-button--lg",
+        ],
       },
       {
         title: "State",
-        values: [`disabled="disabled"`],
+        values: ["gds-button--active", `disabled="disabled"`],
       },
       {
         title: "Display",
-        values: ["gds-button--block-xs"],
+        values: [
+          "gds-button-group__button",
+          "gds-button--block",
+          "gds-button--block-xs",
+          "gds-button--block-sm",
+          "gds-button--block-md",
+          "gds-button--block-lg",
+          "gds-button--block-xl",
+        ],
       },
     ],
     related: [
       {
+        title: "Link",
+        url: "/atoms/link-component",
+      },
+      {
+        title: "Circular Button",
+        url: "/atoms/circular-button-component",
+      },
+      {
         title: "Button Group",
-        url: "/",
+        url: "/molecules/button-group-component",
+      },
+      {
+        title: "Button Dropdown",
+        url: "/molecules/button-dropdown-component",
+      },
+      {
+        title: "Button Toolbar",
+        url: "/molecules/button-toolbar-component",
       },
     ],
 
-    component: `<button className="gds-button gds-button--default">Default button</button>`,
+    code: `<button className="gds-button gds-button--default">Default button</button>`,
+  },
+  {
+    id: "circular-button-component",
+    gumdrops: "",
+    variants: [
+      {
+        title: "Size",
+        values: [
+          "gds-circular-button--xs",
+          "gds-circular-button--sm",
+          "gds-circular-button--lg",
+          "gds-circular-button--xl",
+        ],
+      },
+      {
+        title: "Context (Color)",
+        values: [
+          "gds-circular-button--secondary",
+          "gds-circular-button--success",
+          "gds-circular-button--warning",
+          "gds-circular-button--info",
+          "gds-circular-button--danger",
+          "gds-circular-button--white",
+          "gds-circular-button--toggle",
+          "gds-circular-button--dark",
+          "gds-circular-button--dark-primary",
+          "gds-circular-button--dark-secondary",
+          "gds-circular-button--dark-tertiary",
+          "gds-circular-button--dark-success",
+          "gds-circular-button--dark-info",
+          "gds-circular-button--dark-warning",
+          "gds-circular-button--dark-danger",
+          "gds-circular-button--dark-toggle",
+        ],
+      },
+      {
+        title: "State",
+        values: [
+          "gds-circular-button--active",
+          "gds-circular-button--inactive",
+          `disabled="disabled"`,
+        ],
+      },
+      {
+        title: "Image",
+        values: ["gds-circular-button__image"],
+      },
+    ],
+    related: [
+      {
+        title: "Link",
+        url: "/atoms/link-component",
+      },
+      {
+        title: "Button",
+        url: "/atoms/button-component",
+      },
+      {
+        title: "Button Group",
+        url: "/molecules/button-group-component",
+      },
+      {
+        title: "Button Dropdown",
+        url: "/molecules/button-dropdown-component",
+      },
+      {
+        title: "Button Toolbar",
+        url: "/molecules/button-toolbar-component",
+      },
+    ],
+
+    code: `<div>
+   <button className="gds-circular-button gds-circular-button--primary"><i className="fa fa-fw fa-trash-o"></i></button>
+   <button className="gds-circular-button gds-circular-button--default">
+        <div
+          className="gds-circular-button__image"
+          style={{
+            backgroundImage: 'url("https://assets.ggops.com/images/ken.png")',
+          }}
+        ></div>
+      </button>
+  </div>`,
   },
 ];
