e556cb60b66625fc86eea5a7381b1abb4ce3638a
Lennick Velez <lennick.velez@gumgum.com>
2024-11-13 16:02:25 -0500

    re added styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../public/styles/scss/index.scss';
+import '../styles/scss/index.scss';
+
 import { AppWrapper } from '../context/state';
 import { useRouter } from 'next/router';
 import Header from '../components/layout/Header';
